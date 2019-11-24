231659128c1c2e48ede7cef3aa4c22f3c03802b4
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-11-24 13:25:21 -0500

    Template driven form - assignment 6
    My solution
    section 15, assignment 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8-td-forms-assignment06';
+
+ subscriptionOptions = ['Basic', 'Advanced', 'Pro'];
+ defaultSubscription = "Advanced";
+ user = {
+  email: '',
+  subscription: '',
+  password: ''
+};
+submitted = false;
+
+ @ViewChild('f', {static: false}) userForm: NgForm;
+ 
+ onSubmit() {
+   this.submitted = true;
+   console.log(this.userForm.value);
+
+   this.user.email = this.userForm.value.email;
+   this.user.subscription = this.userForm.value.subscription;
+   this.user.password = this.userForm.value.password;
+
+   this.userForm.reset();
+ }
 }
